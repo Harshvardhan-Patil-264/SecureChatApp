94ce5d33f458fed4f7246e488f0c157f0fbc8e62
Harshvardhan Patil <harsh264patil@gmail.com>
2026-02-26 23:10:22 +0530

    Update db.js

--- server/config/db.js
+++ server/config/db.js
@@ -7,7 +7,7 @@ require('dotenv').config();
 const mysql = require('mysql2/promise');
 
 const pool = mysql.createPool({
-  host: process.env.DB_HOST || '10.202.138.231',
+  host: process.env.DB_HOST || 'localhost',
   user: process.env.DB_USER || 'root',
   password: process.env.DB_PASS || 'pass',
   database: process.env.DB_NAME || 'chatapp',
